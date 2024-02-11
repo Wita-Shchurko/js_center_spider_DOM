d89adb5f25a8fd72b44aefdfdf7887a078c0dc5b
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-02-11 16:11:52 +0200

    make build

---
+++ index.0e2993ee.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top="".concat((e.clientWidth-t.clientWidth)/2,"px"),t.style.left="".concat((e.clientHeight-t.clientHeight)/2,"px");
+//# sourceMappingURL=index.0e2993ee.js.map
